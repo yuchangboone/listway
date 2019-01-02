1a6f6fcff832ae2527362fb58a8f7804e35f8bbc
yucb <yuchangboone@163.com>
2019-01-02 13:21:14 +0800

    初始化

---
+++ doc/db/1schema.sql
@@ -0,0 +1,5 @@
+-- listway 核心表
+create database `listway` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- listway 工作流相关库
+create database `listway_ac` default character set utf8mb4 collate utf8mb4_general_ci;
